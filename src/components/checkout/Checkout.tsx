da23bf8fa291d5dbecc9d6597c72e4952db793b5
lmv <vazquez.leo.m@gmail.com>
2021-05-23 11:39:18 -0300

    Checkout component

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -1,8 +1,22 @@
-import { Flex } from "@chakra-ui/layout";
+import { Flex, Heading } from "@chakra-ui/layout";
 import { useAppSelector } from "../../app/hooks";
 import React from "react";
 
 export const Checkout: React.FC = () => {
-  const products = useAppSelector((state) => state.cart.products);
-  return <Flex>Checkout</Flex>;
+  const formatTotalAmount = (price: number) => {
+    return new Intl.NumberFormat("en-EN", {
+      style: "currency",
+      currency: "USD",
+    }).format(price);
+  };
+
+  const totalPrice = useAppSelector((state) => state.cart.totalPrice);
+
+  return (
+    <Flex>
+      <Heading size="lg">
+        Your total amounts to {formatTotalAmount(totalPrice)}
+      </Heading>
+    </Flex>
+  );
 };
